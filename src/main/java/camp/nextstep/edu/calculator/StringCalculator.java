a062d0305cb5cb98de979091b62dd7c7c8afca5d
kwonheejeong <gmlwjd9405@naver.com>
2019-11-26 16:44:06 +0900

    [step1] feat: 숫자 두 개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환

--- src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
+++ src/main/java/camp/nextstep/edu/calculator/StringCalculator.java
@@ -2,17 +2,44 @@ package camp.nextstep.edu.calculator;
 
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class StringCalculator {
+
+    public static final String COLONS = ",";
 
     public int add(final String text) {
         if (isEmptyOrNull(text)) {
             return 0;
         }
-        return -1;
+        return sum(text);
     }
 
     private boolean isEmptyOrNull(final String text) {
         return StringUtils.isEmpty(text);
+    }
+
+    private int sum(final String text) {
+        List<Integer> numbers = convertIntArrayToList(splitByDelimiter(text, COLONS));
+        return numbers.stream()
+                .mapToInt(Integer::intValue
+                ).sum();
+    }
+
+    private List<Integer> convertIntArrayToList(int[] input) {
+        List<Integer> list = new ArrayList<>();
+        for (int i : input) {
+            list.add(i);
+        }
+        return list;
+    }
+
+    private int[] splitByDelimiter(final String text, final String delimiter) {
+        return Arrays.stream(text.split(delimiter))
+                .mapToInt(Integer::parseInt)
+                .toArray();
     }
 
 }
