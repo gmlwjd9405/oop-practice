ef78712049fbbd01f120f2201647ff217430e42e
kwonheejeong <gmlwjd9405@naver.com>
2019-11-26 16:03:54 +0900

    [step1] test: 빈 문자열 또는 null 값을 입력할 경우 0을 반환

---
+++ src/test/java/camp/nextstep/edu/calculator/StringCalculatorTest.java
@@ -0,0 +1,29 @@
+package camp.nextstep.edu.calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class StringCalculatorTest {
+
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void emptyOrNull(final String text) {
+        assertThat(calculator.add(text)).isZero();
+    }
+
+}
