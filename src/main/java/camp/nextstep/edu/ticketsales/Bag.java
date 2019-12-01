9efd65ef9b36920a7a572650ce284829222c8b72
kwonheejeong <gmlwjd9405@naver.com>
2019-12-01 20:23:41 +0900

    [ticketsales] 각 클래스의 내부 상태에 접근하는 로직을 캡슐화

--- src/main/java/camp/nextstep/edu/ticketsales/Bag.java
+++ src/main/java/camp/nextstep/edu/ticketsales/Bag.java
@@ -5,32 +5,27 @@ public class Bag {
     private Invitation invitation;
     private Ticket ticket;
 
-    public Bag(long amount) {
-        this(amount, null);
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
     }
 
-    public Bag(long amount, Invitation invitation) {
-        this.amount = amount;
-        this.invitation = invitation;
-    }
-
-    public boolean hasInvitation() {
+    private boolean hasInvitation() {
         return invitation != null;
     }
 
-    public boolean hasTicket() {
-        return ticket != null;
-    }
-
-    public void setTicket(Ticket ticket) {
+    private void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
-    public void minusAmount(Long amount) {
+    private void minusAmount(Long amount) {
         this.amount -= amount;
     }
 
-    public void plusAmount(Long amount) {
-        this.amount += amount;
-    }
 }
