f87626953301aac2098ae356bd6ce4a953a6d8c6
kwonheejeong <gmlwjd9405@naver.com>
2019-12-01 21:12:52 +0900

    [moviebookingsystem] 데이터 중심 구현: 데이터 준비

---
+++ src/main/java/camp/nextstep/edu/moviebookingsystem/DiscountConditionType.java
@@ -0,0 +1,6 @@
+package camp.nextstep.edu.moviebookingsystem;
+
+public enum DiscountConditionType {
+    SEQUENCE,   // 순번 조건
+    PERIOD      // 기간 조건
+}
---
+++ src/main/java/camp/nextstep/edu/moviebookingsystem/README.md
@@ -0,0 +1,8 @@
+### moviebookingsystem
+- 오브젝트 책 예제: 온라인 영화 예매 시스템
+
+
+
+---
+
+### :house: [Go Home](https://github.com/gmlwjd9405/oop-practice)
