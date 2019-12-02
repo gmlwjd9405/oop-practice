735c4150dcdc2832f46b8788fdd495ebffee1667
kwonheejeong <gmlwjd9405@naver.com>
2019-12-02 14:42:20 +0900

    [step1] test: junit5의 여러 방법을 이용한 StringCalculatorTest 수정

--- src/test/java/camp/nextstep/edu/calculator/StringCalculatorTest.java
+++ src/test/java/camp/nextstep/edu/calculator/StringCalculatorTest.java
@@ -1,14 +1,15 @@
 package camp.nextstep.edu.calculator;
 
+import camp.nextstep.edu.calculator.exception.NegativeNumberException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.*;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class StringCalculatorTest {
 
@@ -28,44 +29,71 @@ public class StringCalculatorTest {
 
     @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1"})
+    @ValueSource(strings = {"10000", "100", "1"})
     void oneNumber(final String text) {
-        assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
+        assertThat(calculator.add(text)).isEqualTo(Integer.parseInt(text));
     }
 
     @DisplayName(value = "숫자 두 개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1,2"})
-    void twoNumbers(final String text) {
-        assertThat(calculator.add(text)).isSameAs(3);
+    @CsvSource(value = {"1,2,3=6", "10,100=110", "0,0=0"}, delimiter = '=')
+    void twoNumbers(final String text, final int expected) {
+        assertThat(calculator.add(text)).isSameAs(expected);
     }
 
-    @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
+    @DisplayName("구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1,2:3"})
-    void colons(final String text) {
-        assertThat(calculator.add(text)).isSameAs(6);
+    @MethodSource("defaultDelimiterCase")
+    void defaultDelimiterSum(final String text, final int expected) {
+        assertThat(calculator.add(text)).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> defaultDelimiterCase() {
+        return Stream.of(
+                Arguments.of("1,2,3", 6),
+                Arguments.of("2:3:1000", 1005),
+                Arguments.of("100,101:102", 303),
+                Arguments.of("1:2,3:4,5", 15)
+        );
     }
 
     @DisplayName(value = "//와 \n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
     @ParameterizedTest
-    @ValueSource(strings = {"//;\n1;2;3"})
-    void customDelimiter(final String text) {
-        assertThat(calculator.add(text)).isSameAs(6);
+    @MethodSource
+    void customDelimiterSum(final String text, final int expected) {
+        assertThat(calculator.add(text)).isEqualTo(expected);
     }
 
-    @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    private static Stream<Arguments> customDelimiterSum() {
+        return Stream.of(
+                Arguments.of("//\\;\n1;2;3", 6),
+                Arguments.of("//\\;\n0", 0),
+                Arguments.of("//\\;\n1", 1),
+                Arguments.of("//\\|\n10000", 10000),
+                Arguments.of("//\\*\n100*2*3", 105)
+        );
+    }
+
+    @DisplayName(value = "custom 구분자 사이에 빈 문자열을 넣는 경우 NumberFormatException 가 발생한다.")
+    @Test
+    void emptyStringWhenCustomDelimiter() {
+        assertThatExceptionOfType(NumberFormatException.class)
+                .isThrownBy(() -> calculator.add("//\\;\n"));
+    }
+
+    @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 NegativeNumberException 예외 처리를 한다.")
     @Test
     void negative() {
-        assertThatExceptionOfType(RuntimeException.class)
-                .isThrownBy(() -> calculator.add("-1"));
+        assertThatThrownBy(() -> calculator.add("-1"))
+                .isInstanceOf(NegativeNumberException.class)
+                .hasMessageContaining("음수는 허용하지 않습니다.");
     }
 
     @DisplayName(value = "문자열 계산기에 숫자가 아닌 문자를 전달하는 경우 NumberFormatException 예외 처리를 한다.")
-    @Test
-    void notNumeric() {
+    @ParameterizedTest
+    @ValueSource(strings = {"//\\;\n1;a;3", "b", "*", "1:c,2", "1,^"})
+    void notNumeric(String text) {
         assertThatExceptionOfType(NumberFormatException.class)
-                .isThrownBy(() -> calculator.add("s"));
+                .isThrownBy(() -> calculator.add(text));
     }
-
 }
