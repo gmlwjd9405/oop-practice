78864d7e872c2e3ffc3b73f4a781e783f8e511f7
kwonheejeong <gmlwjd9405@naver.com>
2019-12-04 13:48:02 +0900

    [step1] fix: java regex char를 구분자로 사용할 때 \로 escape 처리

--- src/main/java/camp/nextstep/edu/calculator/splitter/CustomSplitter.java
+++ src/main/java/camp/nextstep/edu/calculator/splitter/CustomSplitter.java
@@ -2,6 +2,10 @@ package camp.nextstep.edu.calculator.splitter;
 
 import org.springframework.util.StringUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,6 +14,9 @@ public class CustomSplitter implements Splitter {
     private static final String CUSTOM_DELIMITER_REGEX = "//(.*)\n(.*)";
     private static final int INDEX_OF_DELIMITER = 1;
     private static final int INDEX_OF_VALUE = 2;
+    private static final List<String> REGEX_META_CHAR = new ArrayList<>(
+            Arrays.asList(".", "|", "^", "$", "*", "+", "?", "(", "[", "{", ")")
+    );
 
     private final Pattern CUSTOM_PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEX);
 
@@ -25,7 +32,7 @@ public class CustomSplitter implements Splitter {
         }
         Matcher matcher = CUSTOM_PATTERN.matcher(value);
         if (matcher.find()) {
-            String customDelimiter = matcher.group(INDEX_OF_DELIMITER);
+            String customDelimiter = convertEscapedString(matcher.group(INDEX_OF_DELIMITER));
             return matcher.group(INDEX_OF_VALUE).split(customDelimiter);
         }
         return EMPTY_ARRAY;
@@ -33,5 +40,25 @@ public class CustomSplitter implements Splitter {
 
     private boolean isMatchedPattern(final String value) {
         return CUSTOM_PATTERN.matcher(value).find();
+    }
+
+    private String convertEscapedString(final String customDelimiter) {
+        if (REGEX_META_CHAR.contains(customDelimiter)) {
+            return "\\" + customDelimiter;
+        }
+        return customDelimiter;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CustomSplitter that = (CustomSplitter) o;
+        return Objects.equals(CUSTOM_PATTERN, that.CUSTOM_PATTERN);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(CUSTOM_PATTERN);
     }
 }
